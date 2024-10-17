bf2bd24d26234045e56aaee1d2d9551c39de690c
Brenndo-l <brenndolyu3@gmail.com>
2024-10-17 18:26:02 -0500

    feat: model e component e importação de dados para o html e css dos atributos

---
+++ sgcmapp/src/app/component/agenda/agenda-form/agenda-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agenda-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './agenda-form.component.html',
+  styles: ``
+})
+export class AgendaFormComponent {
+
+}
